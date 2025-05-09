b222c1bb8438940d8259091c47cbe3dfa74ad22d
Melissa <mespleen@gmail.com>
2025-05-09 14:51:32 -0400

    Server.mjs(backend) updated

--- FrontEnd/movie-tracker-frontend/src/App.jsx
+++ FrontEnd/movie-tracker-frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 import './App.css'
+import Watchlist from './pages/Watchlist/Watchlist';
+import MovieShowPage from './pages/MovieShowPage/MovieShowPage';
+import CreateTracker from './pages/CreateTrackerPage/CreateTracker';
 
 function App() {
   return(
     <>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/watchlist" element={<Watchlist/>} />
+      <Route path="/movies" element={<MovieShowPage/>} />
+      <Route path="/create" element={<CreateTracker/>} />
+    </Routes>
     </>
   )
 }

5a13fde0121793e6f8249b3dd8c0bb3c9afd0ac5
Melissa <mespleen@gmail.com>
2025-05-15 09:35:41 -0400

    Updated css for create tracker/create tracker form

--- FrontEnd/movie-tracker-frontend/src/pages/CreateTracker/CreateTracker.jsx
+++ FrontEnd/movie-tracker-frontend/src/pages/CreateTracker/CreateTracker.jsx
@@ -4,10 +4,12 @@ import CreateTrackerForm from '../../components/CreateTrackerForm/CreateTrackerF
 
 export default function CreateTracker() {
     return (
+      <>
     <div className="main">
-        <h1>Movie Tracker</h1>
+        <h1>🎦 Movie Tracker 🎦</h1>
         <p>Create a new tracker or add a movie to your current movie tracker</p>
         <CreateTrackerForm/>
     </div>
+    </>
   )
 }

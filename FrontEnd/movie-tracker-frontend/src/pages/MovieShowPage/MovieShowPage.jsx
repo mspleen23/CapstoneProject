926895bcb3122f097d2601e5689d839b1347f0a4
Melissa <mespleen@gmail.com>
2025-05-15 11:56:44 -0400

    movie show page css done

--- FrontEnd/movie-tracker-frontend/src/pages/MovieShowPage/MovieShowPage.jsx
+++ FrontEnd/movie-tracker-frontend/src/pages/MovieShowPage/MovieShowPage.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect} from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import "./MovieShowPage.css";
 
 export default function MovieShowPage() {
-    const{ id }= useParams();
-    const navigate = useNavigate()
-    const [movie, setMovie] = useState({});
-
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [movie, setMovie] = useState({});
 
   useEffect(() => {
     async function getMovie() {
       try {
-        console.log(id)
+        console.log(id);
         const response = await fetch(`http://localhost:5050/movies/${id}`);
         const data = await response.json();
         // console.log(data)
@@ -24,9 +23,10 @@ export default function MovieShowPage() {
     getMovie();
   }, [id]);
 
-  
   async function handleDelete() {
-    const confirmDelete = window.confirm("Are you sure you want to delete this movie?");
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this movie?"
+    );
     if (!confirmDelete) return;
 
     try {
@@ -35,7 +35,7 @@ export default function MovieShowPage() {
       });
 
       if (response.ok) {
-        navigate("/movieviewpage"); 
+        navigate("/movieviewpage");
       }
     } catch (error) {
       console.error(error);
@@ -43,59 +43,77 @@ export default function MovieShowPage() {
   }
 
   async function handleEdit() {
-    navigate('/editmovie', {state: {movie}});
-    // const updateMovie = {
-    //   ...movie,
-    //   // review: prompt("Enter new review:", movie.review) || movie.review,
-    // };
+    navigate("/editmovie", { state: { movie } });
+    const updateMovie = {
+      ...movie,
+      // review: prompt("Enter new review:", movie.review) || movie.review,
+    };
 
-    // try {
-    //   const response = await fetch(`http://localhost:5050/movies/${id}`, {
-    //     method: "PUT",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify(updateMovie),
-    //   });
+    try {
+      const response = await fetch(`http://localhost:5050/movies/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updateMovie),
+      });
 
-    //   if (response.ok) {
-    //     const data = await response.json();
-    //     setMovie(data);
-    //     alert("Movie updated!");
-    //     navigate(`/movies/${id}`);
-    //     }
-    // } catch (error) {
-    //   console.error(error);
-    // }
+      if (response.ok) {
+        const data = await response.json();
+        setMovie(data);
+        alert("Movie updated!");
+        navigate(`/movies/${id}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
-
-
 
   return (
     <div className="main">
       <header>
-        <h1>Your Movie List!</h1>
-        <p>Feel free to add, delete, update your movies!</p>
+        <h1>🎟️ Movie Show Page 🎟️ </h1>
+        <p>Here is more info on the movie you selected</p>
       </header>
 
-      <div className="movie-container">
-            <div key={movie._id} className="movie-card">
+      <div className="content">
+        <div className="movie-container">
+          <div key={movie._id} className="movie-card">
+            <div className="movie-poster">
               <img src={movie.posterURL} alt={movie.movieName} />
               <h3>{movie.movieName}</h3>
-              <p>{movie.description}</p>
-              <p><strong>Genre:</strong> {movie.genre}</p>
-              <p><strong>Year:</strong> {movie.year}</p>
-              <p><strong>Rating:</strong> {movie.rating || "No rating yet"}</p>
-              <p><strong>Review:</strong> {movie.review || "No review yet"}</p>
-              <p><strong>Watched:</strong>{movie.watched} </p>
-              <a href={movie.trailerURL} target="_blank" rel="noopener noreferrer">Watch Trailer</a>
             </div>
 
-            <div className="buttons">
+            <p>{movie.description}</p>
+            <p>
+              <strong>Genre:</strong> {movie.genre}
+            </p>
+            <p>
+              <strong>Year:</strong> {movie.year}
+            </p>
+            <p>
+              <strong>Rating:</strong> {movie.rating || "No rating yet"}
+            </p>
+            <p>
+              <strong>Review:</strong> {movie.review || "No review yet"}
+            </p>
+            <p>
+              <strong>Watched:</strong>
+              {movie.watched}{" "}
+            </p>
+            <a
+              href={movie.trailerURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Watch Trailer
+            </a>
+          </div>
+
+          <div className="top-buttons">
             <button onClick={handleDelete}>Delete Movie</button>
             <button onClick={handleEdit}>Update Movie</button>
-
           </div>
       </div>
-      </div>
+    </div>
+    </div>
   );
 }
-
